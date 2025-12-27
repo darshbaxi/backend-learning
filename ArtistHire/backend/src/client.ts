97db4555bccc3680dbc6efa6754bf098f92f64aa
darshbaxi <baxidarsh@gmail.com>
2025-12-27 12:26:08 +0530

    schema added

---
+++ ArtistHire/backend/src/client.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient
+}
+
+const prisma = globalForPrisma.prisma || new PrismaClient()
+
+globalForPrisma.prisma = prisma
+
+export default prisma
